af67735440515f31c86021fbe9fb3ead81d58292
Sebastian <sebasuni05@gmail.com>
2024-04-18 19:22:17 -0400

    cv

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Hola Mundo (feliz dia)' });
+  res.render('index', { title: 'CV SEBASTIAN RAMIREZ' });
 });
 
 module.exports = router;
