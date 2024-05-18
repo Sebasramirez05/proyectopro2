68fcaa6810ace642687c5c44c8168d0ac97f9d9f
Sebastian <sebasuni05@gmail.com>
2024-05-17 20:56:47 -0400

    formulario casi listo

--- controllers/ContactosController.js
+++ controllers/ContactosController.js
@@ -26,6 +26,9 @@ class ContactosController {
     const contactos = await this.contactosModel.obtenerAllContactos();
 
     console.log(contactos);
+
+    // Enviar mensaje de confirmacion
+    res.send("Tu mensaje fue enviado con exito");
   }
 }
 
